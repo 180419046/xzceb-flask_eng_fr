ee34c1377e51110ce93dd3e848845f9a99fd15a5
180419046 <180419046a@gmail.com>
2022-09-12 10:34:01 +0000

    Final changes

--- final_project/machinetranslation/tests.py
+++ final_project/machinetranslation/tests.py
@@ -5,14 +5,14 @@ from translator import *
 class TestSquare(unittest.TestCase):
     def test1(self):
         self.assertEqual(englishToFrench("Hello, World"), "Bonjour, Monde")
-        self.assertEqual(englishToFrench("Hello"), "Bonjour") 
+        self.assertNotEqual(englishToFrench("Hello"), "Peter") 
         self.assertEqual(englishToFrench(""), "")  
 
 
 class TestDouble(unittest.TestCase):
-    def test1(self):
+    def test2(self):
         self.assertEqual(frenchToEnglish("Bonjour, Monde"), "Hello, World") 
-        self.assertEqual(frenchToEnglish("Bonjour"), "Hello") 
+        self.assertNotEqual(frenchToEnglish("Bonjour"), "Peter") 
         self.assertEqual(frenchToEnglish(""), "") 
         
 unittest.main()
--- final_project/machinetranslation/translator.py
+++ final_project/machinetranslation/translator.py
@@ -1,19 +1,16 @@
 """
-Week 2
+Title
 """
-import json
+import os
 from ibm_watson import LanguageTranslatorV3
 from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
-import os
 from dotenv import load_dotenv
 
 load_dotenv()
 
 apikey = os.environ['apikey']
 url = os.environ['url']
-text_to_translate = 'Hello, World'
 
-# Prepare the Authenticator
 authenticator = IAMAuthenticator(apikey)
 language_translator = LanguageTranslatorV3(
     version='2018-05-01',
@@ -24,38 +21,22 @@ language_translator.set_service_url(url)
 
 def englishToFrench(englishText):
     """
-    englishToFrench
+    This function translates english to french
     """
-    #write the code here
     if englishText == "":
         return ""
-    model_id = 'en-fr'
-    translation = language_translator.translate(
-    text=englishText,
-    model_id=model_id).get_result()
-    a = json.loads(json.dumps(translation, indent=2, ensure_ascii=False))
-    return a["translations"][0]["translation"]
+    frenchText = language_translator.translate(
+        text=englishText,
+        model_id='en-fr').get_result()
+    return frenchText.get("translations")[0].get("translation")
 
 def frenchToEnglish(frenchText):
     """
-    vice versa
+    This function translates french to english
     """
-    #write the code here
     if frenchText == "":
         return ""
-    model_id = 'fr-en'
-    translation = language_translator.translate(
-    text=frenchText,
-    model_id=model_id).get_result()
-    a = json.loads(json.dumps(translation, indent=2, ensure_ascii=False))
-    return a["translations"][0]["translation"]
-
-print("1. " + englishToFrench(""))
-print("2. " + frenchToEnglish(""))
-# # Translate
-# translation = language_translator.translate(
-#     text=text_to_translate,
-#     model_id=model_id).get_result()
-
-# # Print results
-# print(json.dumps(translation, indent=2, ensure_ascii=False))
+    englishText = language_translator.translate(
+        text=frenchText,
+        model_id='fr-en').get_result()
+    return englishText.get("translations")[0].get("translation")
